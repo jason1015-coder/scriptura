0344d1d5ab12267155b93945d3560185b946dc71
jason <yishun1015@gmail.com>
2026-07-01 18:29:32 +0100

    fix build

---
+++ cmake-build-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,4 @@
+set(__QT_DEPLOY_TARGET_scriptura_tests_FILE /home/jason/scriptura/cmake-build-Debug/tests/scriptura_tests)
+set(__QT_DEPLOY_TARGET_scriptura_tests_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_scriptura_FILE /home/jason/scriptura/cmake-build-Debug/scriptura)
+set(__QT_DEPLOY_TARGET_scriptura_TYPE EXECUTABLE)
